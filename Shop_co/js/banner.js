243eb3a49fbb85d0365cb08c1ca583f9fbfc3123
Kyrylo Pelekhatyi <159331183+programerIT12@users.noreply.github.com>
2026-06-01 14:10:05 +0300

    Delete Shop_co directory

--- Shop_co/js/banner.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector(".header__advertising-massage-cross");
-const menu = document.querySelector(".header__advertising-background");
-
-btn?.addEventListener("click", () => {
-  menu?.classList.toggle("close");
-});
