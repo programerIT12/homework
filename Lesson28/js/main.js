52f385c18c1e1f7742173b38285913643c6d43a7
Kyrylo Pelekhatyi <159331183+programerIT12@users.noreply.github.com>
2026-06-10 23:27:40 +0100

    Add files via upload

---
+++ Lesson28/js/main.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const todoKeys = {
+  id: "id",
+  text: "text",
+  is_completed: "is_completed",
+};
+
+let todos = [];
+
+const errTodoNotFound = (todoId) => `Todo with id ${todoId} not found`;
+
+const getNewTodoId = (todos) => {
+  return (
+    todos.reduce((maxId, todo) => Math.max(maxId, todo[todoKeys.id]), 0) + 1
+  );
+};
+
+const createToDo = (todos, text) => {
+  const newTodo = {
+    [todoKeys.id]: getNewTodoId(todos),
+    [todoKeys.text]: text,
+    [todoKeys.is_completed]: false,
+  };
+  todos.push(newTodo);
+  return todos;
+};
+
+const completeTodoById = (todos, todoId) => {
+  const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return null;
+  }
+  todo[todoKeys.is_completed] = !todo[todoKeys.is_completed];
+  return todo;
+};
+
+const changeTodoById = (todos, todoId, text) => {
+  const todo = todos.find((todo) => todo[todoKeys.id] === todoId);
+  if (!todo) {
+    console.error(errTodoNotFound(todoId));
+    return null;
+  }
+  todo[todoKeys.text] = text;
+  return todo;
+};
+
+const deleteTodoById = (todos, todoId) => {
+  const todoIndex = todos.findIndex((todo) => todo[todoKeys.id] === todoId);
+  if (todoIndex === -1) {
+    console.error(errTodoNotFound(todoId));
+    return todos;
+  }
+  todos.splice(todoIndex, 1);
+  return todos;
+};
