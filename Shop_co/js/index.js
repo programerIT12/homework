2d1b1f69b7170ed6301ea255628c565f178f1cfe
Kyrylo Pelekhatyi <159331183+programerIT12@users.noreply.github.com>
2026-06-21 16:33:52 +0100

    Add files via upload

---
+++ Shop_co/js/index.js
@@ -0,0 +1,8 @@
+import "./myBanner.js";
+import "./myBurger.js";
+import "./myModal.js";
+import "./myModalBuy.js";
+import "./myAllClothes.js";
+import { reviewsSlider } from "./reviews__slider.js";
+
+reviewsSlider();
